24446f7a3f362b30f5d460cea7e9e8eb77c3b388
vagrant <vagrant@ubuntu1804>
2020-10-08 23:07:31 +0000

    Update script

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -10,13 +10,6 @@ set -x
 aws --version
 terraform --version
 
-# Set environment vatiables
-export AWS_ACCESS_KEY_ID=${{ secrets.AwsAccessKeyId }
-export AWS_SECRET_ACCESS_KEY=${{ secrets.AwsSecretAccessKey }}
-export AWS_DEFAULT_REGION="us-east-1"
-export AWS_DEFAULT_OUTPUT="json"
-
 terraform init -reconfigure -input=false
 terraform plan -input=false -var-file=terraform.tfvars
-
 terraform apply -auto-approve -input=false -var-file=terraform.tfvars
