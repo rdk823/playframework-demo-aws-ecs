f7c062a90beaf470b321041093c4ed54822e4938
Roger D <8725314+rdk823@users.noreply.github.com>
2020-10-11 14:32:07 -0700

    Remove ICMP rule

--- modules/ecs/alb.tf
+++ modules/ecs/alb.tf
@@ -91,13 +91,6 @@ resource "aws_security_group" "app_alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 8
-    to_port     = 0
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
